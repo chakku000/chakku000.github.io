7fd13614268158c6211c7c52c1bbcebcc958ccae
chakku <sakuranotoptimized@gmail.com>
2025-03-15 02:02:12 +0900

    remove unused codes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, BIZ_UDPGothic } from "next/font/google";
+import { BIZ_UDPGothic } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 const bizudpgothic = BIZ_UDPGothic({
   weight: ['400', '700'],
